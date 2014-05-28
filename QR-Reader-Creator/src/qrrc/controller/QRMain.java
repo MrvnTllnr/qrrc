accd0efb5a592f9e912f8821fba114243f2e73c1
Marvin <toxic336600@yahoo.de>
2014-05-28 14:15:11 +0200

    Cleaned imports

--- QR-Reader-Creator/src/qrrc/controller/QRMain.java
+++ QR-Reader-Creator/src/qrrc/controller/QRMain.java
@@ -1,11 +1,5 @@
 package qrrc.controller;
 
-import java.io.File;
-import java.util.Observable;
-import java.util.Observer;
-
-import javax.swing.JFileChooser;
-
 import qrrc.view.Window;
 
 public class QRMain{
--- QR-Reader-Creator/src/qrrc/model/QrData.java
+++ QR-Reader-Creator/src/qrrc/model/QrData.java
@@ -8,7 +8,6 @@ import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
 
 public class QrData {
 	
